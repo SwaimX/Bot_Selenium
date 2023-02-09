a4e38b48c0919b26057bfc379fda7a0fa1b06d52
SwaimX <111075374+SwaimX@users.noreply.github.com>
2023-02-09 18:55:27 +0200

    Update accounts

--- cfg.py
+++ cfg.py
@@ -1,2 +1,13 @@
-username = "your instagram username"
-password = "your instagram password"
+accaunts = {
+    "login1":"your login", "password1":"your password",
+    "login2":"your login", "password2":"your password",
+    "login3":"your login", "password3":"your password",
+    "login4":"your login", "password4":"your password",
+    "login5":"your login", "password5":"your password",
+    "login6":"your login", "password6":"your password",
+    "login7":"your login", "password7":"your password",
+    "login8":"your login", "password8":"your password",
+    "login9":"your login", "password9":"your password",
+    "login10":"your login","password10":"your password",
+
+}d"
